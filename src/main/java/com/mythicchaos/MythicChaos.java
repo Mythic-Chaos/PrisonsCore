215883411635c65f6d25cee3ec23cadf7d377ca5
Bradley Gillies <bradleygillies4170@gmail.com>
2020-02-01 23:23:18 +0000

    Added database opening and closing to main methods

--- src/main/java/com/mythicchaos/MythicChaos.java
+++ src/main/java/com/mythicchaos/MythicChaos.java
@@ -1,15 +1,16 @@
 package com.mythicchaos;
 
+import com.mythicchaos.utils.DBManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MythicChaos extends JavaPlugin {
 
     public void onEnable(){
-
+        DBManager.openConnection();
     }
 
     public void onDisable(){
-
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> DBManager.closeConnection()));
     }
 
 }
