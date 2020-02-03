e7c22ab16619520be9df9477ac4aafc280b26372
Bradley Gillies <bradleygillies4170@gmail.com>
2020-02-03 20:28:12 +0000

    Removed testing event

--- src/main/java/com/mythicchaos/MythicChaos.java
+++ src/main/java/com/mythicchaos/MythicChaos.java
@@ -1,10 +1,6 @@
 package com.mythicchaos;
 
 import com.mythicchaos.utils.DBManager;
-import com.mythicchaos.utils.PrisonPlayer;
-import jdk.Exported;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MythicChaos extends JavaPlugin {
@@ -15,11 +11,6 @@ public class MythicChaos extends JavaPlugin {
 
     public void onDisable(){
         Runtime.getRuntime().addShutdownHook(new Thread(() -> DBManager.closeConnection()));
-    }
-
-    @EventHandler
-    public void onJoin(PlayerJoinEvent event){
-        PrisonPlayer player = (PrisonPlayer) event.getPlayer();
     }
 
 }
